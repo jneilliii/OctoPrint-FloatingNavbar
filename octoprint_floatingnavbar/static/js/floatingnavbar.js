3b82ff9892ba559a0dc5a307c5622dc83bdfdc5e
jneilliii <jneilliii+github@gmail.com>
2018-01-23 16:58:32 -0500

    0.2.0
    TouchUI Fixes.

--- octoprint_floatingnavbar/static/js/floatingnavbar.js
+++ octoprint_floatingnavbar/static/js/floatingnavbar.js
@@ -1,25 +1,29 @@
 $(function() {
-    function floatingNavbarViewModel(parameters) {
-        var self = this;
-		
+	function floatingNavbarViewModel(parameters) {
+		var self = this;
+
 		self.navigationViewModel = parameters[0];
-		
-		$("#navbar").toggleClass("navbar-fixed-top navbar-static-top");
-		$("div.container.octoprint-container").css("margin-top","60px");
-    }
 
-    // This is how our plugin registers itself with the application, by adding some configuration
-    // information to the global variable OCTOPRINT_VIEWMODELS
-    OCTOPRINT_VIEWMODELS.push([
-        // This is the constructor to call for instantiating the plugin
-        floatingNavbarViewModel,
+		//Check for TouchUIPlugin, if not loaded apply special class.
+		var htmlId = $("html").attr("id");
+		if (htmlId != "touch") {
+			$("#navbar").toggleClass("navbar-fixed-top navbar-static-top");
+			$("div.container.octoprint-container").css("margin-top","60px");
+		}
+	}
 
-        // This is a list of dependencies to inject into the plugin, the order which you request
-        // here is the order in which the dependencies will be injected into your view model upon
-        // instantiation via the parameters argument
-        ["navigationViewModel"],
+	// This is how our plugin registers itself with the application, by adding some configuration
+	// information to the global variable OCTOPRINT_VIEWMODELS
+	OCTOPRINT_VIEWMODELS.push([
+		// This is the constructor to call for instantiating the plugin
+		floatingNavbarViewModel,
 
-        // Finally, this is the list of selectors for all elements we want this view model to be bound to.
-        []
-    ]);
+		// This is a list of dependencies to inject into the plugin, the order which you request
+		// here is the order in which the dependencies will be injected into your view model upon
+		// instantiation via the parameters argument
+		["navigationViewModel"],
+
+		// Finally, this is the list of selectors for all elements we want this view model to be bound to.
+		[]
+	]);
 });
