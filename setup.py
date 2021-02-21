b63b5a2086d4c4d92b2c347f09e5d78d9954eadb
jneilliii <jneilliii+github@gmail.com>
2021-02-21 14:02:53 -0500

    add fix for fullscreen plugin

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_%s" % plugin_identifier
 plugin_name = "OctoPrint-FloatingNavbar"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.3.5rc1"
+plugin_version = "0.3.6"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
